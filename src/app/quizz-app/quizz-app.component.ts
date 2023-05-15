e5bfdedecae3cd6859beb417f201c4e626086133
dan-lucian <nitadanlucian@outlook.com>
2023-05-15 16:21:25 +0300

    feat: Added quizz mock data

--- src/app/quizz-app/quizz-app.component.ts
+++ src/app/quizz-app/quizz-app.component.ts
@@ -8,17 +8,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./quizz-app.component.scss']
 })
 export class QuizzAppComponent {
-  quizzes: Quiz[] = [{"id":1,"question":"Sample Question 1","answers":["Answer 1-1","Answer 1-2","Answer 1-3"],"correct_answer":1},{"id":2,"question":"Sample Question 2","answers":["Answer 2-1","Answer 2-2","Answer 2-3"],"correct_answer":2},{"id":3,"question":"Sample Question 3","answers":["Answer 3-1","Answer 3-2","Answer 3-3"],"correct_answer":3},{"id":4,"question":"Sample Question 4","answers":["Answer 4-1","Answer 4-2","Answer 4-3"],"correct_answer":1},{"id":5,"question":"Sample Question 5","answers":["Answer 5-1","Answer 5-2","Answer 5-3"],"correct_answer":2}];
+  quizzes: Quiz[] = [{"id":1,"question":"Ca să ajungeți la o perspectivă comuna asupra obiectivelor, trebuie să:","answers":["așteptați ordinele team leader-ului","trageți la sorți","votați și ascultați majoritatea","negociați prezentând argumente"],"correct_answer":1},{"id":2,"question":"Care este elementul cheie pentru implicarea tuturor membrilor în activitățile de echipă?","answers":["oferirea de recompense pentru participare","siguranța psihologică de la nivelul grupului","prezența constantă a leader-ului în timpul activităților de echipă", "amenințarea cu excluderea din echipă"],"correct_answer":2},{"id":3,"question":"Într-un climat de muncă orientat spre excelență,trebuie să stabiliți mai întâi:","answers":["standardele și metodele de lucru","momentele în care se vor lua pauzele","cine este șeful", "recompensele pentru fiecare membru al echipei"],"correct_answer":1},{"id":4,"question":"Prima măsură pecare o poți lua pentru a alege o sursă sigură este:","answers":["să nu citești de pe internet","să te uiți doar la știrile televizate","să verifici atent sursa", "să citești ziarele"],"correct_answer":1},{"id":5,"question":"Pentru a evita informațiile ieșite din actualitate, este indicat:","answers":["să fii mereu la curent cu ce se întâmplă","să verifici în fiecare zi toate sursele","să verifici data publicării", "să afli lucruri doar verbal, de la alții"],"correct_answer":2}];
+  
   selectedAnswers: { [quizId: number]: number } = {};
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get<Quiz[]>('http://localhost:8000/get_all_quizez').subscribe(quizzes => {
-      this.quizzes = quizzes;
-      // Initialize selectedAnswers to -1 for each quiz
-      this.quizzes.forEach(quiz => this.selectedAnswers[quiz.id] = -1);
-    });
+    // this.http.get<Quiz[]>('http://localhost:8000/get_all_quizez').subscribe(quizzes => {
+    //   this.quizzes = quizzes;
+    //   // Initialize selectedAnswers to -1 for each quiz
+    //   this.quizzes.forEach(quiz => this.selectedAnswers[quiz.id] = -1);
+    // });
   }
 
   submit(): void {
